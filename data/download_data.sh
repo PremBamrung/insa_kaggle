626c9ab448e1ac48065c1c6ad727a4719dcf9189
PremBamrung <misterpremsimson@hotmail.fr>
2021-01-27 20:44:40 +0100

    modifying download_data.sh

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,3 @@
-wget -o data.zip https://enisefr-my.sharepoint.com/:u:/g/personal/premchanok_bamrung_enise_fr/ERJCjieFI_NLoB_9hgpJg50B6Gr2htvCBhW7jhys2zWZCA?download=1
+wget -O data.zip https://enisefr-my.sharepoint.com/:u:/g/personal/premchanok_bamrung_enise_fr/ERJCjieFI_NLoB_9hgpJg50B6Gr2htvCBhW7jhys2zWZCA?download=1
 unzip data.zip
 rm data.zip
